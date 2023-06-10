2830c2067334f52f402002284a7d0560389698b2
sbublie <59267927+sbublie@users.noreply.github.com>
2023-06-11 00:43:56 +0200

    Update database control file

--- cp-analyzer/util/database_control.py
+++ cp-analyzer/util/database_control.py
@@ -11,31 +11,29 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-
-""" query = "CREATE TABLE clusters (cluster_id SERIAL PRIMARY KEY, cluster_name VARCHAR(30), center_lat FLOAT, center_long FLOAT);"
+query = "CREATE TABLE clusters (cluster_id SERIAL PRIMARY KEY, cluster_name VARCHAR(30), center_lat FLOAT, center_long FLOAT);"
 cur.execute(query)
 conn.commit()
 
 query = "CREATE TABLE trips (trip_id SERIAL PRIMARY KEY, load FLOAT, origin_lat FLOAT, origin_long FLOAT, destination_lat FLOAT, destination_long FLOAT, timestamp TIMESTAMP, cluster_ID INT REFERENCES clusters(cluster_id));"
 cur.execute(query)
-conn.commit() """
+conn.commit()
 
-""" query = "INSERT INTO clusters (cluster_name, center_lat, center_long) VALUES (%s, %s, %s)"
+query = "INSERT INTO clusters (cluster_name, center_lat, center_long) VALUES (%s, %s, %s)"
 values = ("Test Cluster", 49.0, 8.0)
 cur.execute(query, values)
 conn.commit()
 
-
 query = "INSERT INTO trips (load, origin_lat, origin_long, destination_lat, destination_long, timestamp, cluster_ID) VALUES (%s, %s, %s, %s, %s,  to_timestamp(%s), %s)"
 values = (100.0, 49.0, 9.0, 48.0, 9.0, 1623251200, 1)
 cur.execute(query, values)
-conn.commit() """
+conn.commit()
 
 # Retrieve the data
-cur.execute("SELECT * FROM clusters")
+""" cur.execute("SELECT * FROM clusters")
 rows = cur.fetchall()
 for row in rows:
-    print(row)
+    print(row) """
 
 # Close the database connection
 cur.close()
