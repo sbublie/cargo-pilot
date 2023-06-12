f715e7da7ab6cb12ce6d863b4ca2728de108d0dc
sbublie <mail@sbublies.com>
2023-06-12 22:55:44 +0200

    Add data display

--- cp-control/src/App.tsx
+++ cp-control/src/App.tsx
@@ -1,8 +1,19 @@
-
-import OsmMap from './components/OsmMap';
+import OsmMap from "./components/OsmMap";
+import DataDisplay from "./components/DataDisplay";
 
 function App() {
-  return <div><OsmMap /></div>;
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <DataDisplay />
+        </div>
+        <div className="col">
+          <OsmMap />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
