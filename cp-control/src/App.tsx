5fefb3794e855d6ff62630a81f95bde70ecfcd56
sbublie <mail@sbublies.com>
2023-06-26 19:37:18 +0200

    Add navbar and router

--- cp-control/src/App.tsx
+++ cp-control/src/App.tsx
@@ -1,23 +1,29 @@
 import OsmMap from "./components/OsmMap";
+import { Link, Route, Routes } from "react-router-dom";
+import { Navbar, Nav, Form, FormControl, Button, NavItem } from 'react-bootstrap';
 
 function App() {
-
   return (
     <div className="container text-center">
-{/*       <div className="row">
-        <div className="col">
-          <DataDisplay
-            showEmptyTours={showEmptyTours}
-            setShowEmptyTours={setShowEmptyTours}
-            showMatches={showMatches}
-            setShowMatches={setShowMatches}
-          />
-        </div>
-      </div> */}
-      <h1>Cargo Pilot</h1>
-      <h3>Match View</h3>
+      <Navbar >
+            <Navbar.Brand as={Link} to="/" >Cargo Pilot</Navbar.Brand>
+            <Navbar.Collapse>
+              <Nav className="mr-auto">
+                <NavItem href="/">
+                  <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                </NavItem>
+                <NavItem href="/map">
+                  <Nav.Link as={Link} to="/map" >Map</Nav.Link>
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
       <div className="row">
-        <OsmMap />
+        <Routes>
+          <Route path="/" element={<h1>Moin</h1>} />
+          <Route path="/map" element={<OsmMap />} />
+          
+        </Routes>
       </div>
     </div>
   );
