f6e62cf1acd0384f911dc70df420cd56fe574460
sbublie <mail@sbublies.com>
2023-06-26 19:54:08 +0200

    Fix router

--- cp-control/src/App.tsx
+++ cp-control/src/App.tsx
@@ -1,28 +1,28 @@
 import OsmMap from "./components/OsmMap";
-import { Link, Route, Routes } from "react-router-dom";
-import { Navbar, Nav, Form, FormControl, Button, NavItem } from 'react-bootstrap';
-
+import { Link, NavLink, Routes, Route } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
 function App() {
   return (
     <div className="container text-center">
-      <Navbar >
-            <Navbar.Brand as={Link} to="/" >Cargo Pilot</Navbar.Brand>
-            <Navbar.Collapse>
-              <Nav className="mr-auto">
-                <NavItem href="/">
-                  <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                </NavItem>
-                <NavItem href="/map">
-                  <Nav.Link as={Link} to="/map" >Map</Nav.Link>
-                </NavItem>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+      <Navbar>
+        {/* "Link" in brand component since just redirect is needed */}
+        <Navbar.Brand as={Link} to="/">
+          Cargo Pilot
+        </Navbar.Brand>
+        <Nav>
+          {/* "NavLink" here since "active" class styling is needed */}
+          <Nav.Link as={NavLink} to="/" >
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/map">
+            Map
+          </Nav.Link>
+        </Nav>
+      </Navbar>
       <div className="row">
         <Routes>
           <Route path="/" element={<h1>Moin</h1>} />
           <Route path="/map" element={<OsmMap />} />
-          
         </Routes>
       </div>
     </div>
