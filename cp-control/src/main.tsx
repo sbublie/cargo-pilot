b999901f4c4f4bb5847d5c3a87786ec63c8d401d
sbublie <mail@sbublies.com>
2023-06-26 20:21:49 +0200

    Add meta tag

--- cp-control/src/main.tsx
+++ cp-control/src/main.tsx
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <BrowserRouter>
       <App />
     </BrowserRouter>
