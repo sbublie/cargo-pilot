beb0acb83d8df69edb299dd020f6ec27f46e5978
sbublie <mail@sbublies.com>
2023-06-27 19:09:58 +0200

    Fix env variable error

--- cp-control/deploy/deploy-server.js
+++ cp-control/deploy/deploy-server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const proxy = require("express-http-proxy");
 
-if (process.env.ENABLE_HTTPS == true) {
+if (process.env.ENABLE_HTTPS === "true") {
   const privateKey = fs.readFileSync("./cert/privkey.pem", "utf8");
   const certificate = fs.readFileSync("./cert/cert.pem", "utf8");
   const ca = fs.readFileSync("./cert/chain.pem", "utf8");
@@ -40,7 +40,7 @@ httpServer.listen(80, () => {
   console.log("HTTP Server started");
 });
 
-if (process.env.ENABLE_SSL == true) {
+if (process.env.ENABLE_SSL === "true") {
   const httpsServer = https.createServer(credentials, app);
 
   httpsServer.listen(443, () => {
