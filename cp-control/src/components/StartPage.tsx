af09ade0d8adbdb8fcf81504ba75a0fdf753d9db
sbublie <mail@sbublies.com>
2023-08-23 12:17:00 +0200

    Add placeholder start page

---
+++ cp-control/src/components/StartPage.tsx
@@ -0,0 +1,47 @@
+function StartPage() {
+  return (
+    <div>
+      <>
+        <br />
+        <br />
+        <br />
+        <h1>Welcome to Cargo Pilot</h1>
+        <br />
+
+        <h3>This page is currently being updated. Please come back later!</h3>
+        <br />
+        <br />
+        <hr></hr>
+        <br />
+
+        <h5>
+          Within the research project in cooperation with Universidade de Aveiro
+        </h5>
+        <i>
+          <h5>
+            “Empty and low utilization route driving patterns for transport
+            logistic trucks: identification and optimization of vehicles
+            utilization”
+          </h5>
+        </i>
+        <h5>
+          this website was created to support transport logistic companies to
+          increase the utilization of their existing truck fleets in a
+          sustainable way.
+        </h5>
+        <br />
+        <br />
+        <h2>Center for Digitization in Mobility Systems (ZDM)</h2>
+        <h5>
+          The Center for Digitization in Mobility Systems (ZDM) specializes in
+          digitization in transportation. Students can thus deepen their
+          knowledge in the field of automated and connected driving. For
+          companies, ZDM offers support in the evaluation and deployment of
+          digitization technologies.
+        </h5>
+      </>
+    </div>
+  );
+}
+
+export default StartPage;
