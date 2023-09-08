10f05f63859d14fdcced3a084c634e5c320a9dfe
sbublie <mail@sbublies.com>
2023-09-08 23:03:49 +0200

    Add conversion of city codes

--- cp-analyzer/trip_handler.py
+++ cp-analyzer/trip_handler.py
@@ -1,11 +1,15 @@
 import json
+import time
+import math
 
 from database_handler import DatabaseHandler
 from models import Trip, Location, Offering
-from geopy.geocoders import Nominatim
+import pgeocode
+
 
 # Initialize the Nominatim geocoder
-geolocator = Nominatim(user_agent="geoapiExercises")
+nomi = pgeocode.Nominatim('de')
+
 
 class TripHandler:
     def __init__(self) -> None:
@@ -32,19 +36,33 @@ class TripHandler:
 
     def process_offering_data(self, json_data) -> None:
 
+        with open('german-city-codes.json', "r") as infile:
+            city_codes = json.load(infile)
+
         for offering in json_data:
-            
             offering_dict = json.loads(offering)
 
-            lat = offering_dict['origin']['lat']
-            long = offering_dict['origin']['long']
+            origin_lat = offering_dict['origin']['lat']
+            origin_long = offering_dict['origin']['long']
 
-            if lat is None and long is None and offering_dict['origin']['post_code'] is not None:
-                lat, long = self.__get_location_from_zip_code(offering_dict['origin']['post_code'], offering_dict['origin']['country_code'])
+            # Check if origin_lat and origin_long are None
+            if origin_lat is None and origin_long is None:
+                # Get the postal code from the offering_dict
+                origin_post_code = offering_dict['origin']['post_code']
+
+                # Check if origin_post_code is not None
+                if origin_post_code is not None:
+                    # Try to look up the coordinates in the city_codes dictionary
+                    location = city_codes.get(str(origin_post_code))
+                    if location:
+                        origin_lat = location["lat"]
+                        origin_long = location["long"]
+                    else:
+                        continue
 
             origin_location = {
-                "lat": lat,
-                "long": long,
+                "lat": origin_lat,
+                "long": origin_long,
                 "street": offering_dict['origin']['street'],
                 "zip_code": offering_dict['origin']['post_code'],
                 "city": offering_dict['origin']['city'],
@@ -53,15 +71,27 @@ class TripHandler:
                 "type": "origin"
             }
 
-            lat = offering_dict['destination']['lat']
-            long = offering_dict['destination']['long']
+            destination_lat = offering_dict['destination']['lat']
+            destination_long = offering_dict['destination']['long']
 
-            if lat is None and long is None and offering_dict['origin']['post_code'] is not None:
-                lat, long = self.__get_location_from_zip_code(offering_dict['origin']['post_code'], offering_dict['origin']['country_code'])
+            # Check if destination_lat and destination_long are None
+            if destination_lat is None and destination_long is None:
+                # Get the postal code from the offering_dict
+                destination_post_code = offering_dict['destination']['post_code']
+
+                # Check if destination_post_code is not None
+                if destination_post_code is not None:
+                    # Try to look up the coordinates in the city_codes dictionary
+                    location = city_codes.get(str(destination_post_code))
+                    if location:
+                        destination_lat = location["lat"]
+                        destination_long = location["long"]
+                    else:
+                        continue
 
             destination_location = {
-                "lat": lat,
-                "long": long,
+                "lat": destination_lat,
+                "long": destination_long,
                 "street": offering_dict['destination']['street'],
                 "zip_code": offering_dict['destination']['post_code'],
                 "city": offering_dict['destination']['city'],
@@ -73,6 +103,11 @@ class TripHandler:
             origin_id = self.database_handler.add_location(origin_location)
             destination_id = self.database_handler.add_location(destination_location)
 
+            load_meter = offering_dict['load']['loading_meter']
+            if not load_meter or math.isnan(load_meter):
+                load_meter = 0.0
+
+
             offering = {
                 "customer": offering_dict['customer_id'],
                 "destination_id": destination_id,
@@ -80,24 +115,8 @@ class TripHandler:
                 "source": offering_dict['source'],
                 "vehicle": offering_dict['vehicle_id'],
                 "load_percentage": offering_dict['load']['capacity_percentage'],
-                "load_meter": offering_dict['load']['loading_meter'],
+                "load_meter": load_meter,
                 "load_weight": offering_dict['load']['weight']
             }
 
             self.database_handler.add_offering(offering)
-
-    def __get_location_from_zip_code(self, zip_code, country):
-        try:
-            # Use the geocoder to get location information for the zip code
-            location = geolocator.geocode({"postalcode": zip_code, "country": country})
-            
-            if location:
-                # Extract latitude and longitude
-                latitude = location.latitude
-                longitude = location.longitude
-                return latitude, longitude
-            else:
-                return None, None  # Location not found for the zip code
-        except Exception as e:
-            print(f"Error: {e}")
-            return None, None
