4e0ad7bf15e87272f50551036cd5731f412d9f4b
sbublie <59267927+sbublie@users.noreply.github.com>
2023-09-24 18:51:39 +0200

    CPD-26 Add option to select file to be uploaded

--- cp-uploader/cp-uploader.py
+++ cp-uploader/cp-uploader.py
@@ -1,8 +1,13 @@
 import inquirer
+import os
 
 from env import SHOW_PROMPT
 from input_converter import InputConverter
 from api_handler import APIHandler
+
+input_folder_path = "input"
+file_names = os.listdir(input_folder_path)
+
 
 def main():
 
@@ -24,15 +29,22 @@ def main():
                 message="What is the source of the data?",
                 choices=["Transics", "DB"],
             ),
-            inquirer.Path('file',
-                          message="Enter the file name",
-                          path_type=inquirer.Path.FILE,
-                          exists=True),
+            inquirer.List(
+                "file",
+                message="Select a file",
+                choices=file_names,
+            ),
         ]
         answers = inquirer.prompt(questions)
 
-        processed_data = InputConverter().convert_data_from_file(filename=answers["file"], source=answers["source"], data_type=answers["data_type"], instance=answers["instance"])
-        APIHandler().upload_data(data=processed_data, data_type=answers["data_type"],instance=answers["instance"] )
+        file_path = f"{input_folder_path}/{answers['file']}"
+
+        processed_data = InputConverter().convert_data_from_file(
+            filename=file_path,
+            source=answers["source"],
+            data_type=answers["data_type"],
+            instance=answers["instance"])
+        APIHandler().upload_data(data=processed_data, data_type=answers["data_type"], instance=answers["instance"])
 
 
 if __name__ == "__main__":
