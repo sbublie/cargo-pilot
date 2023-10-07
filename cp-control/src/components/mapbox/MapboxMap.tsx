dd6a38f4a507668271a5e5fb5863f029f0ad73d4
sbublie <59267927+sbublie@users.noreply.github.com>
2023-10-07 17:41:38 +0200

    CPD-36 Move map features to different components

--- cp-control/src/components/mapbox/MapboxMap.tsx
+++ cp-control/src/components/mapbox/MapboxMap.tsx
@@ -1,17 +1,32 @@
 import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import { Button, Container, Row, Col } from "react-bootstrap";
+
 import { useOfferings } from "../../contexts/OfferingsContext";
 import MapSettingsModal from "./MapSettingsModal";
 import MapStatisticsModal from "./MapStatisticsModal";
-import { Button, Container, Row, Col, Spinner } from "react-bootstrap";
+import {
+  addCustomImageToMap,
+  addControlsToMap,
+  setVisibleMapLayers,
+} from "./mapFeatures";
+import { getTripsGeoJson, addTripClusterToMap } from "./tripHandler";
+import {
+  getOfferingsGeoJson,
+  addOfferingClusterToMap,
+} from "./offeringsHandler";
+import {
+  addCityBoundariesToMap,
+  setCityBoundariesGeoJson,
+} from "./clusterHandler";
 
-import mapboxgl from "mapbox-gl";
 import "./mapbox_style.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { setupMapFeatures } from "./mapFeatures";
 
+// Set the Mapbox API token from the .env file
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
-interface Settings {
+export interface Settings {
   mapMode: "cluster" | "offering" | "trip" | "match";
   dataSource: "db" | "transics";
   animateRoutes: boolean;
@@ -22,7 +37,6 @@ function MapboxMap() {
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
   const [showSettingsModal, setShowSettingsModal] = useState(false);
   const [showStatisticsModal, setShowStatisticsModal] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
   const [settings, setSettings] = useState<Settings>({
     mapMode: "cluster",
     dataSource: "db",
@@ -32,7 +46,8 @@ function MapboxMap() {
   const lng = 9.446113815133662;
   const lat = 47.66559693227496;
   const zoom = 9;
-  const { trips, offerings, boundaries } = useOfferings();
+
+  const { offerings, trips, boundaries } = useOfferings();
 
   const handleCloseSettingsModal = () => setShowSettingsModal(false);
   const handleShowSettingsModal = () => setShowSettingsModal(true);
@@ -52,74 +67,36 @@ function MapboxMap() {
       });
 
       mapInstance.on("load", () => {
-        console.log("map loaded");
-        setupMapFeatures(mapInstance, trips, offerings, boundaries);
-
+        addCustomImageToMap(mapInstance);
+        addControlsToMap(mapInstance);
         setMap(mapInstance);
-        if (
-          offerings.length > 0 &&
-          trips.length > 0 &&
-          boundaries.features.length > 0
-        ) {
-          setIsLoading(false);
-        }
       });
 
       // Clean up on unmount
       return () => mapInstance.remove();
     }
-  }, [trips, offerings, boundaries]);
+  }, []);
+
+  useEffect(() => {
+    if (map) {
+      const offeringsGeoJson = getOfferingsGeoJson(offerings);
+      addOfferingClusterToMap(map, offeringsGeoJson);
+
+      const tripsGeoJson = getTripsGeoJson(trips);
+      addTripClusterToMap(map, tripsGeoJson);
+
+      setCityBoundariesGeoJson(boundaries, offerings);
+      addCityBoundariesToMap(map, boundaries);
+
+      setVisibleMapLayers(map, settings);
+    }
+  }, [map, offerings, trips, boundaries, settings]);
 
   const applySettings = (settings: Settings) => {
     setSettings(settings);
     handleCloseSettingsModal();
   };
 
-  useEffect(() => {
-    if (map && boundaries?.features) {
-      const allLayers = [
-        "germany_overlay",
-        "offering_lines",
-        "line-dashed",
-        "offering_markers",
-        "trip_markers",
-        "trip_lines",
-      ];
-
-      const layersByMode = {
-        offering: ["offering_lines", "offering_markers"],
-        cluster: ["germany_overlay"],
-        trip: ["trip_markers", "trip_lines"],
-        match: ["trip_markers", "trip_lines"],
-      };
-
-      const visibleLayers = layersByMode[settings.mapMode] || [];
-
-      if (
-        settings.animateRoutes &&
-        (settings.mapMode === "offering" || settings.mapMode === "trip")
-      ) {
-        visibleLayers.push("line-dashed");
-      }
-
-      allLayers.forEach((layer) => {
-        map.setLayoutProperty(
-          layer,
-          "visibility",
-          visibleLayers.includes(layer) ? "visible" : "none"
-        );
-      });
-
-      if (visibleLayers.includes("line-dashed")) {
-        map.moveLayer("line-dashed", visibleLayers[0]);
-      }
-      if (visibleLayers.includes(settings.mapMode + "_lines")) {
-        map.moveLayer(settings.mapMode + "_lines", visibleLayers[0]);
-      }
-    }
-  }, [map, settings]);
-
-  console.log("load normal");
   return (
     <>
       <MapSettingsModal
@@ -157,38 +134,6 @@ function MapboxMap() {
         </Row>
       </Container>
       <div className="map-container" ref={mapContainerRef} />
-
-      {/* Overlay the spinner if data is still loading */}
-      {isLoading && (
-        <Container
-          fluid
-          className="d-flex align-items-center justify-content-center position-absolute"
-          style={{
-            top: 60,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: 10,
-            background: "rgba(255,255,255,255)",
-          }}
-        >
-          <Row>
-            <Col className="d-flex justify-content-center">
-              <Spinner
-                className="m-3"
-                animation="border"
-                role="status"
-                style={{ width: "3rem", height: "3rem" }}
-              >
-                <span className="visually-hidden">Loading...</span>
-              </Spinner>
-            </Col>
-          </Row>
-          <Row>
-            <h3>Fetching data...</h3>
-          </Row>
-        </Container>
-      )}
     </>
   );
 }
---
+++ cp-control/src/components/mapbox/tripHandler.tsx
@@ -0,0 +1,255 @@
+import { Map as MapboxMap, MapMouseEvent } from "mapbox-gl";
+import mapboxgl from "mapbox-gl";
+import { FeatureCollection, Point, Feature } from "geojson";
+
+interface Trip {
+  id: number;
+  load_weight: number;
+  load_meter: number;
+  load_percentage: number;
+  source: string;
+  origin: {
+    id: number;
+    zip_code: number;
+    city: string;
+    long: number;
+    lat: number;
+    timestamp: number;
+  };
+  destination: {
+    id: number;
+    zip_code: number;
+    city: string;
+    long: number;
+    lat: number;
+    timestamp: number;
+  };
+}
+
+export const getTripsGeoJson = (trips: Trip[]) => {
+  const tripMarkerData: FeatureCollection<Point> = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  trips.forEach((trip) => {
+    const origin_marker: Feature<Point> = {
+      type: "Feature",
+      properties: {
+        color: "green",
+        type: "Origin",
+        id: trip.origin.id,
+        city: trip.origin.city,
+        source: trip.source,
+        timestamp: trip.origin.timestamp,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [trip.origin.long, trip.origin.lat],
+      },
+    };
+
+    const destination_marker: Feature<Point> = {
+      type: "Feature",
+      properties: {
+        color: "red",
+        type: "Destination",
+        city: trip.destination.city,
+        id: trip.destination.id,
+        source: trip.source,
+        timestamp: trip.destination.timestamp,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [trip.destination.long, trip.destination.lat],
+      },
+    };
+
+    tripMarkerData.features.push(origin_marker);
+    tripMarkerData.features.push(destination_marker);
+  });
+
+  return tripMarkerData;
+};
+
+export const addTripsToMap = (map: MapboxMap, trips: FeatureCollection) => {
+  const source = map.getSource("trip_marker_data") as mapboxgl.GeoJSONSource;
+
+  if (!source) {
+    map.addSource(`trip_marker_data`, {
+      type: "geojson",
+      data: trips,
+    });
+
+    map.addLayer({
+      id: "trip_markers",
+      type: "symbol",
+      source: "trip_marker_data",
+      layout: {
+        "icon-image": "custom-marker",
+        "icon-offset": [0, -20],
+      },
+    });
+
+    map.setLayoutProperty("trip_markers", "visibility", "none");
+  } else {
+    source.setData(trips);
+  }
+};
+
+export function addTripClusterToMap(map: MapboxMap, trips: FeatureCollection) {
+  const source = map.getSource("trip_cluster_data") as mapboxgl.GeoJSONSource;
+
+  if (!source) {
+    console.log("add source");
+    map.addSource("trip_cluster_data", {
+      type: "geojson",
+      // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+      // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+      data: trips,
+      cluster: true,
+      clusterMaxZoom: 14, // Max zoom to cluster points on
+      clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+    });
+
+    map.addLayer({
+      id: "trip_clusters",
+      type: "circle",
+      source: "trip_cluster_data",
+      filter: ["has", "point_count"],
+      paint: {
+        // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#51bbd6",
+          100,
+          "#f1f075",
+          750,
+          "#f28cb1",
+        ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      },
+    });
+
+    map.addLayer({
+      id: "trip_cluster_count",
+      type: "symbol",
+      source: "trip_cluster_data",
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": ["get", "point_count_abbreviated"],
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 12,
+      },
+    });
+
+    map.addLayer({
+      id: "unclustered_trip",
+      type: "circle",
+      source: "trip_cluster_data",
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": ["get", "color"],
+        "circle-radius": 10,
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#fff",
+      },
+    });
+
+    map.on("click", "unclustered_trip", (e) => {
+      const feature = e.features?.[0];
+      if (feature) {
+        const dateString = new Date(
+          feature.properties?.timestamp * 1000
+        ).toLocaleString("de-DE");
+
+        // Assert that geometry is of a type that has coordinates
+        const geometry =
+          feature.geometry as mapboxgl.MapboxGeoJSONFeature["geometry"];
+        const description = `<b><h7>Location #${feature.properties?.id}</h7></b><br/>Source: ${feature.properties?.source}<br/>Time: ${dateString}`;
+
+        if (
+          geometry.type !== "GeometryCollection" &&
+          geometry.coordinates &&
+          description
+        ) {
+          if (geometry.type === "Point") {
+            let coordinates = geometry.coordinates.slice() as [number, number];
+
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            new mapboxgl.Popup()
+              .setLngLat(coordinates)
+              .setHTML(description)
+              .addTo(map);
+          } else {
+            console.error("Geometry type is not a Point");
+          }
+        } else {
+          console.error("Feature geometry or properties are undefined");
+        }
+      }
+    });
+
+    // inspect a cluster on click
+    map.on(
+      "click",
+      "trip_clusters",
+      (e: MapMouseEvent & { features?: Array<any> }) => {
+        // Ensure that features exist and it's not an empty array
+        if (!e.features || e.features.length === 0) {
+          return;
+        }
+
+        const clusterId = e.features[0].properties?.cluster_id;
+        const coordinates = e.features[0].geometry?.coordinates;
+
+        // Ensure that clusterId and coordinates exist
+        if (!clusterId || !coordinates) {
+          return;
+        }
+
+        const source = map.getSource(
+          "trip_cluster_data"
+        ) as mapboxgl.GeoJSONSource; // Type cast to GeoJSONSource
+
+        if (!source.getClusterExpansionZoom) {
+          console.error(
+            "Source does not have the method 'getClusterExpansionZoom'."
+          );
+          return;
+        }
+
+        source.getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+
+          map.easeTo({
+            center: coordinates,
+            zoom: zoom,
+          });
+        });
+      }
+    );
+
+    map.on("mouseenter", "unclustered_trip", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+    map.on("mouseleave", "unclustered_trip", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    map.on("mouseenter", "trip_clusters", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+    map.on("mouseleave", "trip_clusters", () => {
+      map.getCanvas().style.cursor = "";
+    });
+  }
+}
